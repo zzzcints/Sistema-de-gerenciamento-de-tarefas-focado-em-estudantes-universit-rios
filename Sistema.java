6dba65843541b2210b2b6e28500956ddea3d3737
zzzcints <cintia013dias@gmail.com>
2026-06-05 20:58:56 -0300

    implementandoclasse sistema

---
+++ Sistema.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class Sistema {
+    private ArrayList<Tarefa> tarefas;
+    private ArrayList<Disciplina> disciplinas;
+
+    public Sistema(){
+        tarefas = new ArrayList<>();
+        disciplinas = new ArrayList<>();
+    }
+
+    public void cadastrarDisciplina(Disciplina d){
+        disciplinas.add(d);
+
+    }
+
+    public void cadastrarTarefa(Tarefa t){
+        tarefas.add(t);
+
+    }
+
+    public void listarTarefas(){
+
+        for(Tarefa t : tarefas){
+            t.exibir();
+        }
+    }
+    
+
+}
--- Tarefa.java
+++ Tarefa.java
@@ -8,19 +8,33 @@ public class Tarefa {
     private String status;
     private Disciplina disciplina;
 
-   
+    public Tarefa(  
+        String nome_tarefa,
+        String descricao,
+        String prioridade,
+        String status;
+        String disciplina
+    ){
+        this.nome_tarefa = nome_tarefa;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.status = status;
+        this.disciplina = disciplina;
+    }
 
-    public String get
 
-
-
-
-
-
-
-
-
-
+    public String get nome_tarefa(){
+        return nome_tarefa;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public String getPrioridade(){
+        return prioridade;
+    }
+    public Disciplina getDisciplina(){
+        return disciplina;
+    }
 
     
 }
