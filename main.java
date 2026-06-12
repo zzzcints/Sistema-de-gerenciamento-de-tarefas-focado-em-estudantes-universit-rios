7099eeac408f513daa63033541f62d759488e37e
zzzcints <cintia013dias@gmail.com>
2026-06-11 22:28:09 -0300

    implementando main.java

--- main.java
+++ main.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
+
+
 public class Main{
+
+
+       public static void main(String[] args) {
+
+         Scanner sc = new Scanner(System.in);
+         Sistema sistema = new Sistema();
+
+        System.out.print("Nome da disciplina: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Nome do professor: ");
+        String professor = sc.nextLine();
+
+       
+    }
+
+
+
+
+
+       }
+
+
+
+
+
+
+
+
 }
 
 
