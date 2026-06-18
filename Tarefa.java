402e5f39a8865f14edc988b1c28fd23fa3990a4a
zzzcints <cintia013dias@gmail.com>
2026-06-18 16:43:44 -0300

    Implementação classe tarefa

--- Tarefa.java
+++ Tarefa.java
@@ -1,18 +1,16 @@
 public class Tarefa {
 
-
-    
     private String nome_tarefa;
     private String descricao;
     private String prioridade;
     private String status;
     private Disciplina disciplina;
 
-    public Tarefa(  
+     public Tarefa(  
         String nome_tarefa,
         String descricao,
         String prioridade,
-        String status;
+        String status,
         String disciplina
     ){
         this.nome_tarefa = nome_tarefa;
@@ -26,14 +24,32 @@ public class Tarefa {
     public String get nome_tarefa(){
         return nome_tarefa;
     }
-    public String getStatus(){
+
+     public String getDescricao() {
+        return descricao;
+    }
+
+     public String getStatus() {
         return status;
     }
+
+  
     public String getPrioridade(){
         return prioridade;
     }
+
+
     public Disciplina getDisciplina(){
         return disciplina;
+    }
+
+    public void exibir() {
+        System.out.println("Tarefa: " + nomeTarefa);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Prioridade: " + prioridade);
+        System.out.println("Status: " + status);
+        System.out.println("Disciplina: " + disciplina.getNome());
+        System.out.println("-------------------------");
     }
 
     
