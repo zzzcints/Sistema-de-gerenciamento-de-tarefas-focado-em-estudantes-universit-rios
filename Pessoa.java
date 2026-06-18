e11c40c66dcf9a4f46e45c87474078637ef9aeec
zzzcints <cintia013dias@gmail.com>
2026-06-18 17:13:54 -0300

    Classe pessoa terminada

---
+++ Pessoa.java
@@ -0,0 +1,17 @@
+public abstract class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public abstract void exibirInfo();
+}
